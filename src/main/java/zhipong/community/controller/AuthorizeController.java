15a8b5b31d94ddfb7eca91b9a6b53142fe6bccab
zipong <applauseforwhom@163.com>
2021-05-18 16:38:23 +0800

    add database record

--- src/main/java/zhipong/community/controller/AuthorizeController.java
+++ src/main/java/zhipong/community/controller/AuthorizeController.java
@@ -7,9 +7,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import zhipong.community.dto.AccessTokenDTO;
 import zhipong.community.dto.GithubUser;
+import zhipong.community.mapper.UserMapper;
+import zhipong.community.model.User;
 import zhipong.community.provider.GithubProvider;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 /**
  * @Author zhipong
@@ -24,13 +27,18 @@ public class AuthorizeController {
     //启动文件时，读取配置，将配置放到Spring的Map里
     @Value("${github.client.id}")
     private String clientID;
+
     @Value("${github.client.secret}")
     private String clientSecret;
+
     @Value("${github.redirect.uri}")
     private String redirectUri;
+
     @Value("${gitee.grant.type}")
     private String grantType;
 
+    @Autowired
+    private UserMapper userMapper;
 
     @GetMapping("/callback")
     public String callBack(@RequestParam(name = "code") String code,
@@ -46,6 +54,13 @@ public class AuthorizeController {
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser((accessToken));
         if(user!=null){
+            User insertUser = new User();
+            insertUser.setToken(UUID.randomUUID().toString());
+            insertUser.setName(user.getName());
+            insertUser.setAccountId(String.valueOf(user.getId()));
+            insertUser.setGmtCreate(System.currentTimeMillis());
+            insertUser.setGmtModified(insertUser.getGmtCreate());
+            userMapper.insert(insertUser);
             request.getSession().setAttribute("user",user);
             return "redirect:/";
         }else{
--- src/main/java/zhipong/community/mapper/UserMapper.java
+++ src/main/java/zhipong/community/mapper/UserMapper.java
@@ -1,8 +1,15 @@
 package zhipong.community.mapper;
 
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import zhipong.community.model.User;
+
 /**
  * @Author zhipong
  * @Date 2021/5/17
  */
-public class UserMapper {
+@Mapper
+public interface UserMapper {
+    @Insert("insert into user (name,account_id,token,gmt_create,gmt_modify) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
+    void insert(User user);
 }
