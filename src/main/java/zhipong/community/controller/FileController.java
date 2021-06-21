c14bef8da7d8816b079f64f5e3581d5eaba09324
zipong <applauseforwhom@163.com>
2021-06-21 22:25:00 +0800

    使用SDK上次文件到UCloud

--- src/main/java/zhipong/community/controller/FileController.java
+++ src/main/java/zhipong/community/controller/FileController.java
@@ -1,11 +1,16 @@
 package zhipong.community.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 import zhipong.community.dto.FileDTO;
+import zhipong.community.provider.UCloudProvider;
 
-import java.util.Objects;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 
 /**
  * @Author zhipong
@@ -13,9 +18,19 @@ import java.util.Objects;
  */
 @Controller
 public class FileController {
+    @Autowired
+    private UCloudProvider uCloudProvider;
+
     @ResponseBody
     @RequestMapping("/file/upload")
-    public FileDTO upload(){
+    public FileDTO upload(HttpServletRequest request){
+        MultipartHttpServletRequest multipartHttpServletRequest=(MultipartHttpServletRequest) request;
+        MultipartFile file = multipartHttpServletRequest.getFile("editormd-image-file");
+        try {
+            uCloudProvider.upload(file.getInputStream(),file.getContentType(),file.getOriginalFilename());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         FileDTO fileDTO = new FileDTO();
         fileDTO.setSuccess(1);
         fileDTO.setUrl("/images/head.png");
